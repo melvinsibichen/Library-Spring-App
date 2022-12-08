f95eb75e2b9e1d0a8ceaa2a4a8bb7e2e0b06b26c
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-09 00:24:30 +0530

    first

---
+++ src/test/java/com/example/LibrarySpringApp/LibrarySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibrarySpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
